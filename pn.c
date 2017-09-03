a91a03827a29ba28d18c6049c0296f83efbe65b7
snekavsb <31505096+snekavsb@users.noreply.github.com>
2017-09-03 18:43:59 +0530

    Rename po.c to pn.c

---
+++ pn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+scanf("%d",&n);
+printf("%d",n);
+if(n>=0)
+{
+printf("positive numbers");
+}
+else
+{
+printf("negative numbers");
+}
+getch();
+}
