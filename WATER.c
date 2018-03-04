b68c5eff26e18818cd02241fda4170c65fc6bf05
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-03-04 16:57:48 +0530

    Create WATER.c

---
+++ WATER.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int i;
+	scanf("%d\n",&i);
+	while(i>=10)
+	{	printf("hydrosphere,oceans");
+		printf("evaporation");
+		printf("condensation,clouds,fog");
+		printf("advenction");
+		printf("precipitation,sublimation");
+		printf("accmulation,snowfelt,meltwater,sublimation,desublimation");
+		printf("surfacerunoff,channelrunoff,resvoir");
+		printf("plantuptake,interception,transpiration");
+		printf("infiltration,percolation");
+	printf("volconicstreams,geysers");
+	
+	}
+	return 0;
+}
